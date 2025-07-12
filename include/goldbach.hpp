720c100c71173e3975040664a3d229855f0481cc
dataplayer12 <jaiyamsharma@gmail.com>
2025-07-12 15:19:26 +0900

    feat: implement faster compute using binary search

--- include/goldbach.hpp
+++ include/goldbach.hpp
@@ -3,8 +3,13 @@
 #include <stdio.h>
 #include <math.h>
 #include <vector>
+#include <string>
 
-#define COMET_LENGTH (25*1000)
+#define COMET_LENGTH (1000000)
+#define NUM_BUCKETS 8192
+constexpr bool verbose=false;
+// ToDo: add logging levels
+
 typedef u_int32_t num;
 
 void fill_xvalues(num* storage);
@@ -16,4 +21,8 @@ Find all prime numbers from `from` to `to` and store them beginning from storage
 */
 void find_primes(const num from, const num to, num* &storage_head, num* storage_start);
 
-num evalute_partition_function(const num xval, num* prime_storage_start);
+num evalute_partition_function(const num xval, num* prime_storage_start, num* &start_hint);
+
+void update_buckets(const num xval, num* prime_storage_start, num* &bucket_positions, num num_buckets);
+
+num evalute_partition_function_recursive(const num xval, num* prime_storage_start, num* b2_positions, const num num_buckets);
--- lib/goldbach/goldbach.cpp
+++ lib/goldbach/goldbach.cpp
@@ -95,7 +95,7 @@ void find_primes(const num from, const num to, num* &storage_head, num* storage_
     printf("Found %d primes between %d and %d\n", primes_found, from, to);
 }
 
-num evalute_partition_function(const num xval, num* prime_storage_start)
+num evalute_partition_function(const num xval, num* prime_storage_start, num* &start_hint)
 {
     if (xval%2)
     {
@@ -107,13 +107,20 @@ num evalute_partition_function(const num xval, num* prime_storage_start)
     num half = (xval >> 1);
     num result=0;
     num* p1 = prime_storage_start;
-    num* p2_min_loc = prime_storage_start + 1;
+    num* p2_min_loc = start_hint;
     num* p2 = p2_min_loc;
+
+    if (*start_hint > half)
+    {
+        printf("Got bad hint. Resetting...");
+        p2_min_loc = prime_storage_start+1;
+    }
 
     while (*p1 <= half)
     {
         while (*p2 < half)
         {
+            start_hint = p2; // strictly less than half
             p2++;
             p2_min_loc = p2;
         }
@@ -124,6 +131,10 @@ num evalute_partition_function(const num xval, num* prime_storage_start)
         {
             if (sum == xval)
             {
+                if constexpr (verbose)
+                {
+                    printf("xval=%d, p1=%d, p2=%d\n", xval, *p1, *p2);
+                }
                 result++;
             }
             p2++;
@@ -134,5 +145,71 @@ num evalute_partition_function(const num xval, num* prime_storage_start)
         p1++;
     }
 
+    return result;
+}
+
+void update_buckets(const num xval, num* prime_storage_start, num* &bucket_positions, num num_buckets)
+{
+    if (xval%2)
+    {
+        // refuse to eval on odd numbers
+        printf("Function update_buckets was called wrongly\n");
+    }
+
+    for (num _bucket=0; _bucket < num_buckets; _bucket++)
+    {
+        num bucket_min_pos = _bucket*xval/num_buckets;
+        while (prime_storage_start[bucket_positions[_bucket]] < bucket_min_pos)
+        {
+            (*(bucket_positions+_bucket))++;
+        }
+    }
+}
+
+num evalute_partition_function_recursive(const num xval, num* prime_storage_start, num* b2_positions, const num num_buckets)
+{
+    if (xval%2)
+    {
+        // refuse to eval on odd numbers
+        printf("Function evalute_partition_function was called wrongly, returning 0\n");
+        return 0;
+    }
+
+    num half = (xval >> 1);
+    num result=0;
+    num* p1 = prime_storage_start;
+
+    while (*p1 <= half)
+    {
+        num p1_bucket = (*p1)*num_buckets/xval;
+        num p2_bucket = num_buckets - 1 - p1_bucket;
+
+        if (p2_bucket < 0)
+        {
+            printf("BUG! p2 bucket is negative for p1=%d, xval=%d\n", *p1, xval); 
+        }
+
+        num* p2 = prime_storage_start + *(b2_positions + p2_bucket);
+        num sum = *p1 + *p2;
+
+        while (sum <= xval)
+        {
+            // printf("xval=%d, sum=%d\n", xval, sum);
+            if (sum==xval)
+            {
+                if constexpr (verbose)
+                {
+                    printf("xval=%d, p1=%d, p2=%d\n", xval, *p1, *p2);
+                }
+
+                result++;
+            }
+            p2++;
+            sum = *p1 + *p2;
+        }
+
+        p1++;
+
+    }
     return result;
 }
